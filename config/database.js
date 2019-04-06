6050c1e07b233e8b62a1dc594a80f2ad316e7b20
Micah-Ella Estacio <micahellae@gmail.com>
2019-04-06 12:09:44 -0400

    database fix

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://mdatabase:mdb@cluster0-n0gqc.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'mdatabase'
+    'url' : 'mongodb+srv://micahella:micahella@cluster0-zfbuo.mongodb.net/test?retryWrites=true
+', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'messages'
 };
